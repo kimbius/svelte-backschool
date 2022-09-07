6b5b0fe1c929cd33b029178ae6016e4c87db7185
Peerawit Piwsawang <peerawit@MacBook-Air-khxng-Peerawit.local>
2022-09-07 22:43:37 +0700

    fix adapter is not function

--- src/routes/student/[student_id]/+page.js
+++ src/routes/student/[student_id]/+page.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+/** @type {import('./$types').PageLoad} */
 export async function load({ params }) {
     return (await axios.get(`https://backapi.bius.kim/${params.student_id}/info`)).data.data
 }
